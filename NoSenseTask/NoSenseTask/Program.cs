275518384563365898b8cf493731b99e611c5df3
RD-433 <radio_man_433@mail.ru>
2020-10-25 19:01:35 +0400

    Second commit

--- NoSenseTask/NoSenseTask/Program.cs
+++ NoSenseTask/NoSenseTask/Program.cs
@@ -10,6 +10,76 @@ namespace NoSenseTask
     {
         static void Main(string[] args)
         {
+
+            string dataInString = Console.ReadLine();
+
+            IEnumerable<string> dataInSeparated = dataInString.Split(',');
+
+            List<int> dataInt = new List<int>();
+
+            foreach (var item in dataInSeparated)
+            {
+                try
+                {
+                    dataInt.Add(Convert.ToInt32(item));
+                }
+                catch (FormatException)
+                {
+
+                }
+
+            }
+
+
+
+            dataInt.ThisDoesNotMakeAnySense(item => ((item > 9) && (item < 100)), item =>
+            {
+                item = 0;
+                Console.WriteLine(Convert.ToString(item) + "\tAction applied!");
+            });
+
+            /*
+            Console.WriteLine();
+            foreach (var item in dataInt)
+            {
+                Console.Write(Convert.ToString(item) + ' ');
+            }
+            */
+            Console.ReadLine();
+
         }
+    }
+
+
+
+    public static class AdditionalClass
+    {
+        public static void ThisDoesNotMakeAnySense<T>(this IEnumerable<T> IEnum, Predicate<T> predicate, Action<T> action)
+        {
+            if (IEnum == null || IEnum.Count() == 0)
+            {
+                Console.WriteLine("BAD");
+                throw new ArgumentNullException("Input data is null!\n");
+            }
+
+
+
+            foreach (var item in IEnum)
+            {
+                if (!predicate(item))
+                {
+                    action(item);
+                }
+                else
+                {
+                    Console.WriteLine(Convert.ToString(item) + "\tData is good!");
+                }
+            }
+
+            //return IEnum;
+
+        }
+
+
     }
 }
